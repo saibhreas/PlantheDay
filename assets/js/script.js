b25d769fdd2c735d188cdfc885b11dcce8dbe588
siobhan <sr@saibhreas.com>
2021-05-16 18:38:02 -0400

    more problems

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,22 @@
-var check = 0;
-var hourEl= $('#hour');
 var timeDispEl= $("#time-display");
-/*
-var nineAm =[09, textArea, saveBtn9];
-var tenAm =[10, textArea, saveBtn10];
-var ElevenAm =[11, textArea, saveBtn11];
-var twelvePm =[12, textArea, saveBtn12];
-var onePm =[01, textArea, saveBtn01];
-var twoPm =[02, textArea, saveBtn02];
-var threePm =[03, textArea, saveBtn03];
-var fourPm =[04, textArea, saveBtn04];
-var fivePm =[05, textArea, saveBtn05];*/
-
-var $text9= $("#text9AM");
-var $text10= $("#text10AM");
-var $text11= $("#text11AM");
-var $text12= $("#text12PM");
-var $text1= $("#text12PM");
-var $text2= $("#text12PM");
-var $text3= $("#text12PM");
-var $text4= $("#text12PM");
-var $text5= $("#text12PM");
-
-$("button").on("click", function(){
-  localStorage.setItem("9AM", ($text9AM.val()))
-
+//changes
+/* making sure everything loads proplerly, in the right order for DOM seraches*/
+$(document).ready(function(){
+  console.log("ready");
 })
-
-
-
+var row9=$(09, text);
+console.log("This is the value of id 09  "+ row9)
+console.log(typeof row9.value);
 
 
 function displayTime() {
+  //console.log("started");
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
-  console.log(typeof rightNow);
+  //console.log(typeof rightNow);
   timeDispEl.text(rightNow);
   var present = rightNow.substring(16,18);
   var check = parseInt(present);
-  console.log("this is check's value ",check);
+  //console.log("this is check's value ",check);
   
   return check;
 }
