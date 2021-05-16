5eeacbd84c771c1cda9bbf83be15e9000b2f5eca
siobhan <sr@saibhreas.com>
2021-05-15 22:28:25 -0400

    publishihng but needs work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 var check = 0;
 var hourEl= $('#hour');
-var timeDispEl= $("#time-display")
+var timeDispEl= $("#time-display");
+/*
+var nineAm =[09, textArea, saveBtn9];
+var tenAm =[10, textArea, saveBtn10];
+var ElevenAm =[11, textArea, saveBtn11];
+var twelvePm =[12, textArea, saveBtn12];
+var onePm =[01, textArea, saveBtn01];
+var twoPm =[02, textArea, saveBtn02];
+var threePm =[03, textArea, saveBtn03];
+var fourPm =[04, textArea, saveBtn04];
+var fivePm =[05, textArea, saveBtn05];*/
 
-/*var blockOfTime =[
-  {hour :"text",
-  activity:"",
-  save-btn:
+/*
+function checknow(check){
+  for (i=0, i<9, i++){
 
-} ]*/
+  }
+}
+*/
+
 
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -15,7 +27,7 @@ function displayTime() {
   timeDispEl.text(rightNow);
   var present = rightNow.substring(16,18);
   var check = parseInt(present);
-  console.log("this is check's value ",check)
+  console.log("this is check's value ",check);
   
   return check;
 }
@@ -24,6 +36,7 @@ function checkpresent(present){
   console.log("Value of Present: " + present);
 }
 
+
 setInterval(displayTime, 1000);
-checkpresent();
+
 
